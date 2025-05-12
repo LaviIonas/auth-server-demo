cd01c6c322f45bbf39cc27c44c752cb5c39da0d4
Lavi Ionas <laviionas772@gmail.com>
2025-05-12 17:25:17 -0400

    fastapi up and running

--- README.md
+++ README.md
@@ -2,4 +2,8 @@
 
 Auth + RBAC microservice with JWT + password reset flow	FastAPI, OAuth2, JWT, bcrypt, Postgres, Redis, RBAC
 
+venv : source venv/bin/activate
+
 vite react : npm run dev
+
+backend : uvicorn app.main:app --reload
---
+++ backend/app/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from app.db.session import engine
+from app.db.base import Base
+
+app = FastAPI()
+
+@app.on_event("startup")
+def startup():
+    Base.metadata.create_all(bind=engine)
+
+@app.get("/")
+def root():
+    return {"message": "Auth server is alive"}
